72c49c9afafa2ef6d4bf7a391a43e5346e45fe7b
Emanuel Rico Conde <ricocondee@gmail.com>
2023-05-17 21:45:52 -0500

    Product information finally obtained and rendered, useEffect and localStorage was implemented.

--- src/pages/FullProduct.jsx
+++ src/pages/FullProduct.jsx
@@ -1,11 +1,33 @@
-import React from "react";
-import Skeleton from "react-loading-skeleton";
+import { React, useEffect, useState } from "react";
 import fullProductStyles from "../styles/fullProduct.module.css";
+import { handleProductData } from "../hooks/useGetProducts"; //importp a modo hook la funcion abstracta para obtener el producto seleccionado
 
 const FullProduct = () => {
+  const [productData, setProductData] = useState(null);//estado para producto seleccionado
+  useEffect(() => {
+    const getSelectedProductData = async () => { // funcion asincrona para obtener la info del producto selecionado
+      try {
+        const item = await handleProductData(localStorage.getItem("itemID"));//espero por la data y la capturo con ayuda del localstorage
+        setProductData(item);//seteo el valor
+      } catch (err) {
+        console.table(err);
+      }
+    };
+    getSelectedProductData();//llamo a la funcion
+  }, []);
+  //A ESTE PUNTO LO QUE QUEDA ES MAQUETAR BIEN, YA LOS DATOS ESTAN SIENDO OBTENIDOS//
   return (
     <div className={fullProductStyles.container}>
-      
+      {productData?.map((prod) => (
+        <div>
+          <img src={prod.image} alt="" />
+          <form action="">
+            <h1>{prod.name}</h1>
+            <p>{prod.name} {prod.color} {prod.rom} {prod.ram}</p>
+            <span>{prod.price}</span>
+          </form>
+        </div>
+      ))}
     </div>
   );
 };
