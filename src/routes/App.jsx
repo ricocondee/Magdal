7805a37eb45d23ec1760f62dc61dfbca141234f8
Jonier Ballestas <ballestasjoniermiguel@litoral.edu.co>
2023-05-28 20:07:35 -0500

    Trabajando en el Login

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -5,7 +5,7 @@ import useInitialState from "../hooks/useInitialState";
 import AppContext from "../context/AppContext";
 import Home from '../pages/Home'
 import FullProduct from "../pages/FullProduct";
-
+import Login from "../components/Login";
 const App = () => {
   const initialState = useInitialState();
   return (
@@ -15,6 +15,7 @@ const App = () => {
           <Routes>
             <Route exact path="/" element={<Home />}/>
             <Route exact path="/product" element={<FullProduct />}/>
+            <Route exact path="/login" element={<Login />}></Route>
           </Routes>
         </Layout>
       </BrowserRouter>
